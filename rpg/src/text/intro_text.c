2f2bff5e0772b5878d924ce3aee85ef17e1ac669
Harleen <harleen.singh-kaur@epitech.eu>
2023-04-19 23:55:15 +0200

    [FIX] modify function who print dialogue

--- rpg/src/text/intro_text.c
+++ rpg/src/text/intro_text.c
@@ -11,20 +11,21 @@
 #include "rpg.h"
 #include "struct.h"
 
-void init_intro_text(txt_t *t)
+void init_intro_text(text_t *txt)
 {
     char name[100] = "./rpg/assets/font/KGBlankSpaceSketch.ttf";
     sfFont *f = sfFont_createFromFile(name);
+    sfVector2f pos_txt = {250, 900};
     char *intro_buffer = get_buffer("./rpg/assets/config/context.txt");
     char **tab = my_str_to_word_array(intro_buffer, '\n');
 
     for (int i = 0; tab[i] != NULL; i++) {
-        t[i].text = sfText_create();
-        sfText_setString(t[i].text, tab[i]);
-        sfText_setFont(t[i].text, f);
-        sfText_setCharacterSize(t[i].text, 24);
-        sfText_setFillColor(t[i].text, sfWhite);
-        sfText_setPosition(t[i].text, (sfVector2f){250, 900});
+        txt[i].text = sfText_create();
+        sfText_setString(txt[i].text, tab[i]);
+        sfText_setFont(txt[i].text, f);
+        sfText_setCharacterSize(txt[i].text, 24);
+        sfText_setFillColor(txt[i].text, sfWhite);
+        sfText_setPosition(txt[i].text, pos_txt);
     }
     free(intro_buffer);
     free_tab(tab);
