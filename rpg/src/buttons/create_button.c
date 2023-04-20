ae95c122bed166722dd1ae4166ea340e46eda65d
OrionPaX4k <thomas.lebouc@epitech.eu>
2023-04-20 22:29:29 +0200

    [EDIT] bg

--- rpg/src/buttons/create_button.c
+++ rpg/src/buttons/create_button.c
@@ -15,8 +15,9 @@ void set_button_new_game(btn_t *btn)
     btn[0].pos.y = 270;
     btn[0].rect = sfRectangleShape_create();
     sfRectangleShape_setSize(btn[0].rect, btn[0].scale);
+    sfRectangleShape_setFillColor(btn[0].rect, sfTransparent);
     sfRectangleShape_setPosition(btn[0].rect, btn[0].pos);
-    sfRectangleShape_setOutlineColor(btn[0].rect, sfBlack);
+    sfRectangleShape_setOutlineColor(btn[0].rect, sfTransparent);
     sfRectangleShape_setOutlineThickness(btn[0].rect, 2.f);
 }
 
@@ -28,8 +29,9 @@ void set_button_savegarde(btn_t *btn)
     btn[1].pos.y = 450;
     btn[1].rect = sfRectangleShape_create();
     sfRectangleShape_setSize(btn[1].rect, btn[1].scale);
+    sfRectangleShape_setFillColor(btn[1].rect, sfTransparent);
     sfRectangleShape_setPosition(btn[1].rect, btn[1].pos);
-    sfRectangleShape_setOutlineColor(btn[1].rect, sfBlack);
+    sfRectangleShape_setOutlineColor(btn[1].rect, sfTransparent);
     sfRectangleShape_setOutlineThickness(btn[1].rect, 2.f);
 }
 
@@ -41,8 +43,9 @@ void set_button_credit(btn_t *btn)
     btn[2].pos.y = 630;
     btn[2].rect = sfRectangleShape_create();
     sfRectangleShape_setSize(btn[2].rect, btn[2].scale);
+    sfRectangleShape_setFillColor(btn[2].rect, sfTransparent);
     sfRectangleShape_setPosition(btn[2].rect, btn[2].pos);
-    sfRectangleShape_setOutlineColor(btn[2].rect, sfBlack);
+    sfRectangleShape_setOutlineColor(btn[2].rect, sfTransparent);
     sfRectangleShape_setOutlineThickness(btn[2].rect, 2.f);
 }
 
@@ -54,8 +57,9 @@ void set_button_quit(btn_t *btn)
     btn[3].pos.y = 810;
     btn[3].rect = sfRectangleShape_create();
     sfRectangleShape_setSize(btn[3].rect, btn[3].scale);
+    sfRectangleShape_setFillColor(btn[3].rect, sfTransparent);
     sfRectangleShape_setPosition(btn[3].rect, btn[3].pos);
-    sfRectangleShape_setOutlineColor(btn[3].rect, sfBlack);
+    sfRectangleShape_setOutlineColor(btn[3].rect, sfTransparent);
     sfRectangleShape_setOutlineThickness(btn[3].rect, 2.f);
 }
 
@@ -67,6 +71,7 @@ void set_button_setting(btn_t *btn)
     btn[4].pos.y = 50;
     btn[4].rect = sfRectangleShape_create();
     sfRectangleShape_setSize(btn[4].rect, btn[4].scale);
+    sfRectangleShape_setFillColor(btn[4].rect, sfTransparent);
     sfRectangleShape_setPosition(btn[4].rect, btn[4].pos);
     sfRectangleShape_setOutlineColor(btn[4].rect, sfBlack);
     sfRectangleShape_setOutlineThickness(btn[4].rect, 2.f);
